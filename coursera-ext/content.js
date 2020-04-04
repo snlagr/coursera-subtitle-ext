02a316a947187f4f909d7638f93803d25b109ec0
Sonal Agrawal <37601228+coolsonu39@users.noreply.github.com>
2020-04-04 23:48:40 +0530

    improved redability

--- coursera-ext/content.js
+++ coursera-ext/content.js
@@ -3,20 +3,26 @@ document.onkeypress = function(e) {
 
     if(e.key == 'c' || e.key == 'C') {
 
-		// grab all subtitles
-		subList = document.getElementsByClassName("subtitle-label");
+	// grab all subtitles
+	subList = document.getElementsByClassName("subtitle-label");
 
-		// find out engSubs and noSubs
-		for (i=0; i<subList.length; i++)
-		    if(subList[i].innerHTML == "Subtitles Off")
-		        noSubs = subList[i];
-		    else if(subList[i].innerHTML == "English")
-		       	engSubs = subList[i];
+	// find out engSubs and noSubs
+	for (i=0; i<subList.length; i++) {
+	    if(subList[i].innerHTML == "Subtitles Off") {
+		noSubs = subList[i];
+	    }
+	    else if(subList[i].innerHTML == "English") {
+		engSubs = subList[i];
+	    }
+	}
 
-		// toggling logic
-        if(noSubs.parentElement.parentElement.className == "active")
+	// toggling logic
+        if(noSubs.parentElement.parentElement.className == "active"){
         	engSubs.parentElement.click();
-        else noSubs.parentElement.click();
+	}
+        else {
+		noSubs.parentElement.click();
+	}
         
     }
 }
